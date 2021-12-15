27a49451abf0d45dc932be1f5da45c24f358f568
Viet Anh Nguyen <vietanh.dev@gmail.com>
2021-12-16 00:56:02 +0700

    Update new daisykitsdk

--- app/src/main/jni/daisykit_camera.h
+++ app/src/main/jni/daisykit_camera.h
@@ -17,13 +17,13 @@
 #include "asset_mgr_utils.h"
 #include "third_party/ndkcamera/ndkcamera.h"
 
-#include <daisykitsdk/common/types.h>
-#include <daisykitsdk/flows/face_detector_flow.h>
-#include <daisykitsdk/flows/background_matting_flow.h>
-#include <daisykitsdk/flows/barcode_scanner_flow.h>
-#include <daisykitsdk/flows/human_pose_movenet_flow.h>
-#include <daisykitsdk/flows/hand_pose_detector_flow.h>
-#include <daisykitsdk/flows/object_detector_flow.h>
+#include <daisykit/common/types.h>
+#include <daisykit/flows/face_detector_flow.h>
+#include <daisykit/flows/background_matting_flow.h>
+#include <daisykit/flows/barcode_scanner_flow.h>
+#include <daisykit/flows/human_pose_movenet_flow.h>
+#include <daisykit/flows/hand_pose_detector_flow.h>
+#include <daisykit/flows/object_detector_flow.h>
 
 
 static std::string read_file_from_assets(AAssetManager* mgr, const std::string &file_name) {
